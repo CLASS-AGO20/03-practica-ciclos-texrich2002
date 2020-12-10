973110b56101df807fd7573d25fc5bae2c62c3cd
texrich2002 <60866652+texrich2002@users.noreply.github.com>
2020-12-10 15:38:38 -0600

    factorial de un numero

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-export default class App {}
+export default class App {
+factorial(numero){
+    let i,total=0;
+    for(i=numero; i=0; i--){
+        total = i*i;
+    }
+    return total;
 
+
+}
+
+}
 let app = new App();
+console.log("Factorial de un numero: ");
+console.log(app.factorial(5));
+console.log("");
