8111d228e11d270fd513becea51af426c3a3f78b
texrich2002 <60866652+texrich2002@users.noreply.github.com>
2020-12-10 15:55:46 -0600

    funcion convertir a string

--- src/main.js
+++ src/main.js
@@ -1,16 +1,35 @@
 export default class App {
 factorial(numero){
-    let i,total=0;
-    for(i=numero; i=0; i--){
-        total = i*i;
+    let i,total=1;
+    for(i=1; i<=numero; i++){
+        total = total*i;
     }
     return total;
+}
+convertirAString(numero){
+    let i=numero;
+    let string = "";
+    while(i > 0){
+        string += '*';
+        i--;
+   }
+   return string;
+}
+obtenerDivisibles(numero){
+    let i=1;
+    do {
+        if(i % 3 == 0)
+            string += i.toString();
 
-
+        i++;
+    }while(i<=numero);
 }
 
 }
 let app = new App();
 console.log("Factorial de un numero: ");
 console.log(app.factorial(5));
+console.log("");
+console.log("Convertir numero a string: ");
+console.log(app.convertirAString(5));
 console.log("");
